370a090a2f8c7c91db8fa2a110ac8dafc845c3a4
GoksuKO <goksuk@gmail.com>
2026-01-02 01:54:17 +0100

    going to start with backtracking

--- 03_Optimization/L3/hospitals/hospitals.py
+++ 03_Optimization/L3/hospitals/hospitals.py
@@ -1,5 +1,7 @@
 import random
 
+# HILL-CLIMBING AND RANDOM RESTART FOR HOSPITAL PLACEMENT
+
 class Space():
 
     def __init__(self, height, width, num_hospitals):
--- 03_Optimization/L3/production/production.py
+++ 03_Optimization/L3/production/production.py
@@ -1,9 +1,17 @@
 import scipy.optimize
 
-# Objective Function: 50x_1 + 80x_2
-# Constraint 1: 5x_1 + 2x_2 <= 20
-# Constraint 2: -10x_1 + -12x_2 <= -90
+# LINEAR PROGRAMMING EXAMPLE
 
+# Two machines X1 and X2. X1 costs $50/hour to run, X2 costs $80/hour to run. Goal is to minimize cost.
+## Objective Function: 50x_1 + 80x_2
+
+# X1 requires 5 units of labor per hour. X2 requires 2 units of labor per hour. Total of 20 units of labor to spend.
+## Constraint 1: 5x_1 + 2x_2 <= 20
+
+# X1 produces 10 units of output per hour. X2 produces 12 units of output per hour. Company needs 90 units of output.
+## Constraint 2: -10x_1 + -12x_2 <= -90
+
+# ub indicates upper bound constraints
 result = scipy.optimize.linprog(
     [50, 80],  # Cost function: 50x_1 + 80x_2
     A_ub=[[5, 2], [-10, -12]],  # Coefficients for inequalities
@@ -15,3 +23,4 @@ if result.success:
     print(f"X2: {round(result.x[1], 2)} hours")
 else:
     print("No solution")
+
